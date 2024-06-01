cc47bd937434c5a3c771608ffc7afcb6f3e16d47
adamocallaghan <adam.ocallaghan@yahoo.ie>
2024-06-01 14:57:49 +0100

    Fixed Exercise added and tests started

--- src/exercise/FixedExercise.sol
+++ src/exercise/FixedExercise.sol
@@ -7,20 +7,20 @@ import {SafeERC20} from "oz/token/ERC20/utils/SafeERC20.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
 import {BaseExercise} from "../exercise/BaseExercise.sol";
-import {IOracle} from "../interfaces/IOracle.sol";
 import {OptionsToken} from "../OptionsToken.sol";
 
-struct DiscountExerciseParams {
+struct FixedExerciseParams {
     uint256 maxPaymentAmount;
     uint256 deadline;
 }
 
-/// @title Options Token Exercise Contract
-/// @author @lookee, @eidolon
+/// @title Options Token Fixed Price Exercise Contract
+/// @author @adamo, @funkornaut
 /// @notice Contract that allows the holder of options tokens to exercise them,
-/// in this case, by purchasing the underlying token at a discount to the market price.
+/// in this case, by purchasing the underlying token at a discount to a fixed
+/// price set by owner.
 /// @dev Assumes the underlying token and the payment token both use 18 decimals.
-contract DiscountExercise is BaseExercise {
+contract FixedExercise is BaseExercise {
     /// Library usage
     using SafeERC20 for IERC20;
     using FixedPointMathLib for uint256;
@@ -29,13 +29,15 @@ contract DiscountExercise is BaseExercise {
     error Exercise__SlippageTooHigh();
     error Exercise__PastDeadline();
     error Exercise__MultiplierOutOfRange();
-    error Exercise__InvalidOracle();
+    error Exercise__ExerciseWindowNotOpen();
+    error Exercise__ExerciseWindowClosed();
 
     /// Events
     event Exercised(address indexed sender, address indexed recipient, uint256 amount, uint256 paymentAmount);
-    event SetOracle(IOracle indexed newOracle);
+    event SetPriceAndTimeWindow(uint256 indexed price, uint256 indexed startTime, uint256 endTime);
     event SetTreasury(address indexed newTreasury);
     event SetMultiplier(uint256 indexed newMultiplier);
+    event SetPrice(uint256 indexed price);
 
     /// Constants
 
@@ -53,24 +55,31 @@ contract DiscountExercise is BaseExercise {
 
     /// Storage variables
 
-    /// @notice The oracle contract that provides the current price to purchase
-    /// the underlying token while exercising options (the strike price)
-    IOracle public oracle;
-
-    /// @notice The multiplier applied to the TWAP value. Encodes the discount of
+    /// @notice The multiplier applied to the price. Encodes the discount of
     /// the options token. Uses 4 decimals.
     uint256 public multiplier;
 
+    /// @notice The time after which users can exercise their option tokens
+    uint256 public startTime;
+
+    /// @notice The time after which users can no longer exercise their option tokens
+    uint256 public endTime;
+
+    /// @notice The fixed token price, set by the owner
+    uint256 public price;
+
     /// @notice The amount of payment tokens the user can claim
     /// Used when the contract does not have enough tokens to pay the user
-    mapping (address => uint256) public credit;
+    mapping(address => uint256) public credit;
 
     constructor(
         OptionsToken oToken_,
         address owner_,
         IERC20 paymentToken_,
         IERC20 underlyingToken_,
-        IOracle oracle_,
+        uint256 price_,
+        uint256 startTime_,
+        uint256 endTime_,
         uint256 multiplier_,
         address[] memory feeRecipients_,
         uint256[] memory feeBPS_
@@ -78,16 +87,18 @@ contract DiscountExercise is BaseExercise {
         paymentToken = paymentToken_;
         underlyingToken = underlyingToken_;
 
-        _setOracle(oracle_);
+        price = price_;
+        startTime = startTime_;
+        endTime = endTime_;
+
         _setMultiplier(multiplier_);
 
-        emit SetOracle(oracle_);
+        emit SetPriceAndTimeWindow(price, startTime, endTime);
     }
 
     /// External functions
 
     /// @notice Exercises options tokens to purchase the underlying tokens.
-    /// @dev The oracle may revert if it cannot give a secure result.
     /// @param from The user that is exercising their options tokens
     /// @param amount The amount of options tokens to exercise
     /// @param recipient The recipient of the purchased underlying tokens
@@ -111,20 +122,6 @@ contract DiscountExercise is BaseExercise {
 
     /// Owner functions
 
-    /// @notice Sets the oracle contract. Only callable by the owner.
-    /// @param oracle_ The new oracle contract
-    function setOracle(IOracle oracle_) external onlyOwner {
-        _setOracle(oracle_);
-    }
-
-    function _setOracle(IOracle oracle_) internal {
-        (address paymentToken_, address underlyingToken_) = oracle_.getTokens();
-        if (paymentToken_ != address(paymentToken) || underlyingToken_ != address(underlyingToken))
-            revert Exercise__InvalidOracle();
-        oracle = oracle_;
-        emit SetOracle(oracle_);
-    }
-
     /// @notice Sets the discount multiplier.
     /// @param multiplier_ The new multiplier
     function setMultiplier(uint256 multiplier_) external onlyOwner {
@@ -140,6 +137,16 @@ contract DiscountExercise is BaseExercise {
         emit SetMultiplier(multiplier_);
     }
 
+    /// @notice Sets the fixed token price
+    function setPrice(uint256 price_) external onlyOwner {
+        _setPrice(price_);
+    }
+
+    function _setPrice(uint256 price_) internal {
+        price = price_;
+        emit SetPrice(price);
+    }
+
     /// Internal functions
 
     function _exercise(address from, uint256 amount, address recipient, bytes memory params)
@@ -147,13 +154,14 @@ contract DiscountExercise is BaseExercise {
         virtual
         returns (uint256 paymentAmount, address, uint256, uint256)
     {
+        // check if exercise window is open
+        if (block.timestamp < startTime) revert Exercise__ExerciseWindowNotOpen();
+        if (block.timestamp > endTime) revert Exercise__ExerciseWindowClosed();
+
         // decode params
-        DiscountExerciseParams memory _params = abi.decode(params, (DiscountExerciseParams));
+        FixedExerciseParams memory _params = abi.decode(params, (FixedExerciseParams));
 
         if (block.timestamp > _params.deadline) revert Exercise__PastDeadline();
-
-        // apply multiplier to price
-        uint256 price = oracle.getPrice().mulDivUp(multiplier, MULTIPLIER_DENOM);
 
         paymentAmount = amount.mulWadUp(price);
         if (paymentAmount > _params.maxPaymentAmount) revert Exercise__SlippageTooHigh();
@@ -182,6 +190,6 @@ contract DiscountExercise is BaseExercise {
     /// @notice Returns the amount of payment tokens required to exercise the given amount of options tokens.
     /// @param amount The amount of options tokens to exercise
     function getPaymentAmount(uint256 amount) external view returns (uint256 paymentAmount) {
-        paymentAmount = amount.mulWadUp(oracle.getPrice().mulDivUp(multiplier, MULTIPLIER_DENOM));
+        paymentAmount = amount.mulWadUp(price.mulDivUp(multiplier, MULTIPLIER_DENOM));
     }
 }
--- test/FixedOptionsToken.t.sol
+++ test/FixedOptionsToken.t.sol
@@ -8,21 +8,16 @@ import {IERC20} from "oz/token/ERC20/IERC20.sol";
 import {ERC1967Proxy} from "oz/proxy/ERC1967/ERC1967Proxy.sol";
 
 import {OptionsToken} from "../src/OptionsToken.sol";
-import {DiscountExerciseParams, DiscountExercise, BaseExercise} from "../src/exercise/DiscountExercise.sol";
+import {FixedExerciseParams, FixedExercise, BaseExercise} from "../src/exercise/FixedExercise.sol";
 import {TestERC20} from "./mocks/TestERC20.sol";
-import {BalancerOracle} from "../src/oracles/BalancerOracle.sol";
-import {MockBalancerTwapOracle} from "./mocks/MockBalancerTwapOracle.sol";
 
-contract OptionsTokenTest is Test {
+contract FixedOptionsTokenTest is Test {
     using FixedPointMathLib for uint256;
 
     uint16 constant PRICE_MULTIPLIER = 5000; // 0.5
-    uint56 constant ORACLE_SECS = 30 minutes;
-    uint56 constant ORACLE_AGO = 2 minutes;
-    uint128 constant ORACLE_MIN_PRICE = 1e17;
-    uint56 constant ORACLE_LARGEST_SAFETY_WINDOW = 24 hours;
-    uint256 constant ORACLE_INIT_TWAP_VALUE = 1e19;
-    uint256 constant ORACLE_MIN_PRICE_DENOM = 10000;
+    uint56 constant SECS = 30 minutes;
+    uint128 constant MIN_PRICE = 1e17;
+    uint256 constant MIN_PRICE_DENOM = 10000;
     uint256 constant MAX_SUPPLY = 1e27; // the max supply of the options token & the underlying token
 
     address owner;
@@ -31,11 +26,13 @@ contract OptionsTokenTest is Test {
     uint256[] feeBPS_;
 
     OptionsToken optionsToken;
-    DiscountExercise exerciser;
-    BalancerOracle oracle;
-    MockBalancerTwapOracle balancerTwapOracle;
+    FixedExercise exerciser;
     TestERC20 paymentToken;
     address underlyingToken;
+
+    uint256 price;
+    uint256 startTime;
+    uint256 endTime;
 
     function setUp() public {
         // set up accounts
@@ -64,12 +61,22 @@ contract OptionsTokenTest is Test {
         tokens[0] = address(paymentToken);
         tokens[1] = underlyingToken;
 
-        balancerTwapOracle = new MockBalancerTwapOracle(tokens);
-        console.log(tokens[0], tokens[1]);
-        oracle = new BalancerOracle(balancerTwapOracle, underlyingToken, owner, ORACLE_SECS, ORACLE_AGO, ORACLE_MIN_PRICE);
+        price = 1e18;
+        startTime = block.timestamp + 1 hours;
+        endTime = block.timestamp + 4 weeks;
 
-        exerciser =
-        new DiscountExercise(optionsToken, owner, IERC20(address(paymentToken)), IERC20(underlyingToken), oracle, PRICE_MULTIPLIER, feeRecipients_, feeBPS_);
+        exerciser = new FixedExercise(
+            optionsToken,
+            owner,
+            IERC20(address(paymentToken)),
+            IERC20(underlyingToken),
+            price,
+            startTime,
+            endTime,
+            PRICE_MULTIPLIER,
+            feeRecipients_,
+            feeBPS_
+        );
 
         TestERC20(underlyingToken).mint(address(exerciser), 1e20 ether);
 
@@ -79,7 +86,6 @@ contract OptionsTokenTest is Test {
         vm.stopPrank();
 
         // set up contracts
-        balancerTwapOracle.setTwapValue(ORACLE_INIT_TWAP_VALUE);
         paymentToken.approve(address(exerciser), type(uint256).max);
     }
 
@@ -101,6 +107,8 @@ contract OptionsTokenTest is Test {
     }
 
     function test_exerciseHappyPath(uint256 amount, address recipient) public {
+        openExerciseWindow();
+
         amount = bound(amount, 100, MAX_SUPPLY);
 
         // mint options tokens
@@ -108,11 +116,11 @@ contract OptionsTokenTest is Test {
         optionsToken.mint(address(this), amount);
 
         // mint payment tokens
-        uint256 expectedPaymentAmount = amount.mulWadUp(ORACLE_INIT_TWAP_VALUE.mulDivUp(PRICE_MULTIPLIER, ORACLE_MIN_PRICE_DENOM));
+        uint256 expectedPaymentAmount = amount.mulWadUp(price.mulDivUp(PRICE_MULTIPLIER, MIN_PRICE_DENOM));
         paymentToken.mint(address(this), expectedPaymentAmount);
 
         // exercise options tokens
-        DiscountExerciseParams memory params = DiscountExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
+        FixedExerciseParams memory params = FixedExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
         (uint256 paymentAmount,,,) = optionsToken.exercise(amount, recipient, address(exerciser), abi.encode(params));
 
         // verify options tokens were transferred
@@ -128,27 +136,29 @@ contract OptionsTokenTest is Test {
         assertEqDecimal(paymentAmount, expectedPaymentAmount, 18, "exercise returned wrong value");
     }
 
-    function test_exerciseMinPrice(uint256 amount, address recipient) public {
-        amount = bound(amount, 1, MAX_SUPPLY);
+    // function test_exerciseMinPrice(uint256 amount, address recipient) public {
+    //     amount = bound(amount, 1, MAX_SUPPLY);
 
-        // mint options tokens
-        vm.prank(tokenAdmin);
-        optionsToken.mint(address(this), amount);
+    //     // mint options tokens
+    //     vm.prank(tokenAdmin);
+    //     optionsToken.mint(address(this), amount);
 
-        // set TWAP value such that the strike price is below the oracle's minPrice value
-        balancerTwapOracle.setTwapValue(ORACLE_MIN_PRICE - 1);
+    //     // set TWAP value such that the strike price is below the oracle's minPrice value
+    //     balancerTwapOracle.setTwapValue(MIN_PRICE - 1);
 
-        // mint payment tokens
-        uint256 expectedPaymentAmount = amount.mulWadUp(ORACLE_MIN_PRICE);
-        paymentToken.mint(address(this), expectedPaymentAmount);
+    //     // mint payment tokens
+    //     uint256 expectedPaymentAmount = amount.mulWadUp(MIN_PRICE);
+    //     paymentToken.mint(address(this), expectedPaymentAmount);
 
-        // exercise options tokens
-        DiscountExerciseParams memory params = DiscountExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
-        vm.expectRevert(bytes4(keccak256("BalancerOracle__BelowMinPrice()")));
-        optionsToken.exercise(amount, recipient, address(exerciser), abi.encode(params));
-    }
+    //     // exercise options tokens
+    //     FixedExerciseParams memory params = FixedExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
+    //     vm.expectRevert(bytes4(keccak256("BalancerOracle__BelowMinPrice()")));
+    //     optionsToken.exercise(amount, recipient, address(exerciser), abi.encode(params));
+    // }
 
     function test_priceMultiplier(uint256 amount, uint256 multiplier) public {
+        openExerciseWindow();
+
         amount = bound(amount, 1, MAX_SUPPLY / 2);
 
         vm.prank(owner);
@@ -159,11 +169,11 @@ contract OptionsTokenTest is Test {
         optionsToken.mint(address(this), amount * 2);
 
         // mint payment tokens
-        uint256 expectedPaymentAmount = amount.mulWadUp(ORACLE_INIT_TWAP_VALUE);
+        uint256 expectedPaymentAmount = amount.mulWadUp(price);
         paymentToken.mint(address(this), expectedPaymentAmount);
 
         // exercise options tokens
-        DiscountExerciseParams memory params = DiscountExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
+        FixedExerciseParams memory params = FixedExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
         (uint256 paidAmount,,,) = optionsToken.exercise(amount, address(this), address(exerciser), abi.encode(params));
 
         // update multiplier
@@ -172,7 +182,7 @@ contract OptionsTokenTest is Test {
         exerciser.setMultiplier(multiplier);
 
         // exercise options tokens
-        uint256 newPrice = oracle.getPrice().mulDivUp(multiplier, 10000);
+        uint256 newPrice = price.mulDivUp(multiplier, 10000);
         uint256 newExpectedPaymentAmount = amount.mulWadUp(newPrice);
         params.maxPaymentAmount = newExpectedPaymentAmount;
 
@@ -184,6 +194,8 @@ contract OptionsTokenTest is Test {
     }
 
     function test_exerciseHighSlippage(uint256 amount, address recipient) public {
+        openExerciseWindow();
+
         amount = bound(amount, 1, MAX_SUPPLY);
 
         // mint options tokens
@@ -191,39 +203,41 @@ contract OptionsTokenTest is Test {
         optionsToken.mint(address(this), amount);
 
         // mint payment tokens
-        uint256 expectedPaymentAmount = amount.mulWadUp(ORACLE_INIT_TWAP_VALUE.mulDivUp(PRICE_MULTIPLIER, ORACLE_MIN_PRICE_DENOM));
+        uint256 expectedPaymentAmount = amount.mulWadUp(price.mulDivUp(PRICE_MULTIPLIER, MIN_PRICE_DENOM));
         paymentToken.mint(address(this), expectedPaymentAmount);
 
         // exercise options tokens which should fail
-        DiscountExerciseParams memory params = DiscountExerciseParams({maxPaymentAmount: expectedPaymentAmount - 1, deadline: type(uint256).max});
-        vm.expectRevert(DiscountExercise.Exercise__SlippageTooHigh.selector);
+        FixedExerciseParams memory params = FixedExerciseParams({maxPaymentAmount: expectedPaymentAmount - 1, deadline: type(uint256).max});
+        vm.expectRevert(FixedExercise.Exercise__SlippageTooHigh.selector);
         optionsToken.exercise(amount, recipient, address(exerciser), abi.encode(params));
     }
 
-    function test_exerciseTwapOracleNotReady(uint256 amount, address recipient) public {
-        amount = bound(amount, 1, MAX_SUPPLY);
+    // function test_exerciseTwapOracleNotReady(uint256 amount, address recipient) public {
+    //     amount = bound(amount, 1, MAX_SUPPLY);
 
-        // mint options tokens
-        vm.prank(tokenAdmin);
-        optionsToken.mint(address(this), amount);
+    //     // mint options tokens
+    //     vm.prank(tokenAdmin);
+    //     optionsToken.mint(address(this), amount);
 
-        // mint payment tokens
-        uint256 expectedPaymentAmount = amount.mulWadUp(ORACLE_INIT_TWAP_VALUE.mulDivUp(PRICE_MULTIPLIER, ORACLE_MIN_PRICE_DENOM));
-        paymentToken.mint(address(this), expectedPaymentAmount);
+    //     // mint payment tokens
+    //     uint256 expectedPaymentAmount = amount.mulWadUp(price.mulDivUp(PRICE_MULTIPLIER, MIN_PRICE_DENOM));
+    //     paymentToken.mint(address(this), expectedPaymentAmount);
 
-        // update oracle params
-        // such that the TWAP window becomes (block.timestamp - ORACLE_LARGEST_SAFETY_WINDOW - ORACLE_SECS, block.timestamp - ORACLE_LARGEST_SAFETY_WINDOW]
-        // which is outside of the largest safety window
-        vm.prank(owner);
-        oracle.setParams(ORACLE_SECS, ORACLE_LARGEST_SAFETY_WINDOW, ORACLE_MIN_PRICE);
+    //     // update oracle params
+    //     // such that the TWAP window becomes (block.timestamp - ORACLE_LARGEST_SAFETY_WINDOW - SECS, block.timestamp - ORACLE_LARGEST_SAFETY_WINDOW]
+    //     // which is outside of the largest safety window
+    //     vm.prank(owner);
+    //     oracle.setParams(SECS, ORACLE_LARGEST_SAFETY_WINDOW, MIN_PRICE);
 
-        // exercise options tokens which should fail
-        DiscountExerciseParams memory params = DiscountExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
-        vm.expectRevert(BalancerOracle.BalancerOracle__TWAPOracleNotReady.selector);
-        optionsToken.exercise(amount, recipient, address(exerciser), abi.encode(params));
-    }
+    //     // exercise options tokens which should fail
+    //     FixedExerciseParams memory params = FixedExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
+    //     vm.expectRevert(BalancerOracle.BalancerOracle__TWAPOracleNotReady.selector);
+    //     optionsToken.exercise(amount, recipient, address(exerciser), abi.encode(params));
+    // }
 
     function test_exercisePastDeadline(uint256 amount, address recipient, uint256 deadline) public {
+        openExerciseWindow();
+
         amount = bound(amount, 0, MAX_SUPPLY);
         deadline = bound(deadline, 0, block.timestamp - 1);
 
@@ -232,13 +246,13 @@ contract OptionsTokenTest is Test {
         optionsToken.mint(address(this), amount);
 
         // mint payment tokens
-        uint256 expectedPaymentAmount = amount.mulWadUp(ORACLE_INIT_TWAP_VALUE.mulDivUp(PRICE_MULTIPLIER, ORACLE_MIN_PRICE_DENOM));
+        uint256 expectedPaymentAmount = amount.mulWadUp(price.mulDivUp(PRICE_MULTIPLIER, MIN_PRICE_DENOM));
         paymentToken.mint(address(this), expectedPaymentAmount);
 
         // exercise options tokens
-        DiscountExerciseParams memory params = DiscountExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: deadline});
+        FixedExerciseParams memory params = FixedExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: deadline});
         if (amount != 0) {
-            vm.expectRevert(DiscountExercise.Exercise__PastDeadline.selector);
+            vm.expectRevert(FixedExercise.Exercise__PastDeadline.selector);
         }
         optionsToken.exercise(amount, recipient, address(exerciser), abi.encode(params));
     }
@@ -250,11 +264,11 @@ contract OptionsTokenTest is Test {
         vm.prank(tokenAdmin);
         optionsToken.mint(address(this), amount);
 
-        uint256 expectedPaymentAmount = amount.mulWadUp(ORACLE_INIT_TWAP_VALUE.mulDivUp(PRICE_MULTIPLIER, ORACLE_MIN_PRICE_DENOM));
+        uint256 expectedPaymentAmount = amount.mulWadUp(price.mulDivUp(PRICE_MULTIPLIER, MIN_PRICE_DENOM));
         paymentToken.mint(address(this), expectedPaymentAmount);
 
         // exercise options tokens which should fail
-        DiscountExerciseParams memory params = DiscountExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
+        FixedExerciseParams memory params = FixedExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
         vm.expectRevert(BaseExercise.Exercise__NotOToken.selector);
         exerciser.exercise(address(this), amount, recipient, abi.encode(params));
     }
@@ -271,12 +285,16 @@ contract OptionsTokenTest is Test {
         optionsToken.setExerciseContract(address(exerciser), false);
 
         // mint payment tokens
-        uint256 expectedPaymentAmount = amount.mulWadUp(ORACLE_INIT_TWAP_VALUE.mulDivUp(PRICE_MULTIPLIER, ORACLE_MIN_PRICE_DENOM));
+        uint256 expectedPaymentAmount = amount.mulWadUp(price.mulDivUp(PRICE_MULTIPLIER, MIN_PRICE_DENOM));
         paymentToken.mint(address(this), expectedPaymentAmount);
 
         // exercise options tokens which should fail
-        DiscountExerciseParams memory params = DiscountExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
+        FixedExerciseParams memory params = FixedExerciseParams({maxPaymentAmount: expectedPaymentAmount, deadline: type(uint256).max});
         vm.expectRevert(OptionsToken.OptionsToken__NotExerciseContract.selector);
         optionsToken.exercise(amount, recipient, address(exerciser), abi.encode(params));
+    }
+
+    function openExerciseWindow() public {
+        vm.warp(block.timestamp + 2 hours);
     }
 }
